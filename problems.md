d6554cd2f0da8cf4f9bc351f845d90e0253aa835
jazzhands <egod09080908@yandex.ru>
2025-11-29 20:21:27 +0300

    removed all files to start from scratch

--- problems.md
+++
@@ -1,4 +0,0 @@
-# I2C configuration
-I want to configure I2C, for which i need to configure clocks.
-## Only Dividers?
-I have to setup dividers?
--- src/RP2040/general_defines.h
+++
@@ -1,9 +0,0 @@
-#ifndef GENERAL_DEFINES_H
-#define GENERAL_DEFINES_H
-
-void PUT32 ( unsigned int, unsigned int ); // defined in assembly code
-unsigned int GET32 ( unsigned int ); // defined in assembly code
-
-void write_masked(unsigned int reg, unsigned int value, unsigned int mask);
-
-#endif // !GENERAL_DEFINES_H
