54beff546c4b0c566446f93b2f4e505ebff58b98
jazzhands <egod09080908@yandex.ru>
2025-11-30 18:03:14 +0300

    figured out compilation with sdk

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
-
+#include "bsp/board_api.h"
+#include "class/hid/hid_device.h"
+#include "device/usbd.h"
+#include "tusb.h"
 
 int main() {
+    board_init();
+    tud_init(BOARD_TUD_RHPORT);
+    while(1) {
+        tud_task();
+    }
 
 }
